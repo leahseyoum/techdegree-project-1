66d0551bd40b157d3dd65945d6945ef9546a7c22
Leah Seyoum <leahseyoum@gmail.com>
2024-10-28 14:13:52 -0700

    exceeds requirements

--- project/src/main/java/GuessingGame.java
+++ project/src/main/java/GuessingGame.java
@@ -1,42 +1,38 @@
-import java.util.Scanner;
-
 public class GuessingGame {
     public static void main(String[] args) {
+		
+		Prompter prompter = new Prompter();
 			
-			Scanner itemNameObj = new Scanner(System.in);
-			System.out.println("What type of item should fill the jar?  ");
-			String itemName = itemNameObj.nextLine();
-			
-			Scanner itemNumberObj = new Scanner(System.in);
-			System.out.printf("What is the maximum amount of %s?  ", itemName);
-			String itemNumberString = itemNumberObj.nextLine();
-			int maxItemNumber = Integer.parseInt(itemNumberString);
+
+		String itemName = prompter.promptItemName();
+		
+
+		int maxItemNumber = prompter.promptItemNumber(itemName);
 			
 			// New jar instance
 			Jar jar = new Jar(itemName, maxItemNumber);
 			jar.fill();
 			
-			// First guess
-			Scanner guessObj = new Scanner(System.in);
-			System.out.printf("%n%n%n How many %s are in the jar? Pick a number between 1 and %d:  ",itemName, maxItemNumber);
-			String stringGuess = guessObj.nextLine();
-			int guess = Integer.parseInt(stringGuess);
-			
 			// Game loop
-			int numberOfGuesses = 1;
-			while (guess != jar.getCurrentNumberOfItems()) {
-				Scanner newGuessObj = new Scanner(System.in);
-				System.out.printf("%nHow many %s are in the jar? Pick a number between 1 and %d:  ",itemName, maxItemNumber);
-				String newStringGuess = newGuessObj.nextLine();
-				guess = Integer.parseInt(newStringGuess);
+			int numberOfGuesses = 0;
+			int guess;
+			int currentNum = jar.getCurrentNumberOfItems();
+			do {
+				guess = prompter.promptForGuess(itemName, maxItemNumber);
+				
+				if (guess > maxItemNumber) {
+					guess = prompter.promptForValidGuess(guess, maxItemNumber);
+				} 
+				if (guess > currentNum || guess < currentNum) {
+					prompter.promptForClue(guess, currentNum);
+				}
 				numberOfGuesses++;
 				
-				if (guess == jar.getCurrentNumberOfItems()) break;
-					
-			}
+			} while(guess != currentNum);
 			
+		
 			// Completion message
-			System.out.printf("Congrats! You got it in %d attempts%n", numberOfGuesses);
+			prompter.promptForWin(numberOfGuesses);
 				
     }
 }
---
+++ project/src/main/java/Prompter.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Prompter {
+	
+	private final Scanner scanner;
+	
+	public Prompter() {
+		this.scanner = new Scanner(System.in);
+	}
+	
+	public String promptItemName() {
+			System.out.println(" What type of item should fill the jar?  ");
+			return scanner.nextLine();
+	}
+	
+	public int promptItemNumber (String itemName) {
+			System.out.printf("%n What is the maximum amount of %s?  ", itemName);
+			String itemNumberString = scanner.nextLine();
+			return Integer.parseInt(itemNumberString);
+	}
+	
+	public int promptForGuess(String itemName, int maxItemNumber) {
+			System.out.printf("%n How many %s are in the jar? Pick a number between 1 and %d:  ",itemName, maxItemNumber);
+			String stringGuess = scanner.nextLine();
+			return Integer.parseInt(stringGuess);
+	}
+	
+	public int promptForValidGuess(int guess, int maxItemNumber) {
+		
+		while (guess > maxItemNumber) {
+			System.out.print("%n Your guess must be less than maximum fill amount:  ");
+			String stringGuess = scanner.nextLine();
+			guess = Integer.parseInt(stringGuess);
+		}
+		
+		return guess;
+	}
+	
+	public void promptForClue(int guess, int currentNum) {
+		if (guess < currentNum) {
+			System.out.println(" Your guess is too low");
+		} else if (guess > currentNum) {
+			System.out.println(" Your guess is too high");
+		}
+	}
+	
+	public void promptForWin(int numberOfGuesses) {
+		System.out.printf(" Congrats! You got it in %d attempts%n", numberOfGuesses);
+	}
+}
